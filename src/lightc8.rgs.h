46e6c28f1afdc2cd8c5c52eb0ca4a21ba74f79f5
icebarf <sysgrammer@protonmail.com>
2023-10-29 20:16:31 +0530

    new UI theme style

--- src/lightc8.rgs.h
+++ src/lightc8.rgs.h
@@ -11,18 +11,19 @@
 //                                                                              //
 //////////////////////////////////////////////////////////////////////////////////
 
-#define LIGHTC8_STYLE_PROPS_COUNT  11
+#define LIGHTC8_STYLE_PROPS_COUNT  12
 
 // Custom style name: LightC8
 static const GuiStyleProp lightc8StyleProps[LIGHTC8_STYLE_PROPS_COUNT] = {
-    { 3, 0, 0x030303ff },    // TOGGLE_BORDER_COLOR_NORMAL 
-    { 3, 1, 0x060606ff },    // TOGGLE_BASE_COLOR_NORMAL 
-    { 3, 2, 0x040404ff },    // TOGGLE_TEXT_COLOR_NORMAL 
-    { 3, 4, 0x4f4e4eff },    // TOGGLE_BASE_COLOR_FOCUSED 
-    { 3, 5, 0x515151ff },    // TOGGLE_TEXT_COLOR_FOCUSED 
-    { 3, 6, 0x696969ff },    // TOGGLE_BORDER_COLOR_PRESSED 
-    { 3, 7, 0xfbf9f9ff },    // TOGGLE_BASE_COLOR_PRESSED 
-    { 3, 8, 0xfffdfdff },    // TOGGLE_TEXT_COLOR_PRESSED 
+    { 3, 0, 0x676767ff },    // TOGGLE_BORDER_COLOR_NORMAL 
+    { 3, 1, 0x2e9df6ff },    // TOGGLE_BASE_COLOR_NORMAL 
+    { 3, 2, 0x56d9ecff },    // TOGGLE_TEXT_COLOR_NORMAL 
+    { 3, 3, 0x5c5c5cff },    // TOGGLE_BORDER_COLOR_FOCUSED 
+    { 3, 4, 0x75d1e9ff },    // TOGGLE_BASE_COLOR_FOCUSED 
+    { 3, 5, 0x457f89ff },    // TOGGLE_TEXT_COLOR_FOCUSED 
+    { 3, 6, 0x5a5a5aff },    // TOGGLE_BORDER_COLOR_PRESSED 
+    { 3, 7, 0xeae9e9ff },    // TOGGLE_BASE_COLOR_PRESSED 
+    { 3, 8, 0xf0efefff },    // TOGGLE_TEXT_COLOR_PRESSED 
     { 3, 9, 0x040404ff },    // TOGGLE_BORDER_COLOR_DISABLED 
     { 3, 10, 0x010101ff },    // TOGGLE_BASE_COLOR_DISABLED 
     { 3, 11, 0x010101ff },    // TOGGLE_TEXT_COLOR_DISABLED 
